67a9e65bddf8cb89ec3b4ccb905ac1582a37b176
Christopher Dancy <chris.dancy@decentlabs.io>
2022-08-15 16:10:08 -0400

    created a wrapper contract to work with MVD

---
+++ contracts/GnosisWrapper.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.8.0;
+import "@fractal-framework/core-contracts/contracts/ModuleBase.sol";
+import "./IGnosisWrapper.sol";
+
+contract GnosisWrapper is ModuleBase {
+    address public gnosisSafe;
+
+    function initialize(address _accessControl, address _gnosisSafe)
+        public
+        initializer
+    {
+        __initBase(_accessControl, msg.sender, "Wrapper Module");
+        _registerInterface(type(IGnosisWrapper).interfaceId);
+        gnosisSafe = _gnosisSafe;
+    }
+}

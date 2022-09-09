fe4d7d117e228f19b0970654c934bfb1727fef9d
tbwebb22 <tbwebb22@gmail.com>
2022-09-09 16:19:05 -0400

    VetoGuard contract initial commit

---
+++ contracts/VetoGuard.sol
@@ -0,0 +1,302 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "@gnosis.pm/zodiac/contracts/guard/BaseGuard.sol";
+import "@gnosis.pm/zodiac/contracts/factory/FactoryFriendly.sol";
+import "@gnosis.pm/safe-contracts/contracts/common/Enum.sol";
+import "@gnosis.pm/safe-contracts/contracts/GnosisSafe.sol";
+
+contract VetoGuard is FactoryFriendly, BaseGuard {
+    enum TransactionState {
+        pending, // 0
+        queued, // 1
+        readyToExecute, // 2
+        vetoed // 3
+    }
+
+    struct TransactionData {
+        TransactionState state;
+        uint256 queuedBlock;
+    }
+
+    event VetoGuardSetup(address indexed creator, address indexed owner);
+
+    // address public gnosisSafe;
+    uint256 public executionDelayBlocks;
+    mapping(bytes32 => TransactionData) transactionData;
+
+    // todo: need to look into these two values and if we need it in this contract
+    bytes32 private constant SAFE_TX_TYPEHASH =
+        0xbb8310d486368db6bd6f849402fdd73ad53d316b5a4b2644ad6efe0f941286d8;
+    bytes32 private constant DOMAIN_SEPARATOR_TYPEHASH =
+        0x47e79534a245952e8b16893a336b85a3d9ea9fa8c573f3d803afb92a79469218;
+
+    constructor(address _owner, uint256 _executionDelayBlocks) {
+        bytes memory initializeParams = abi.encode(
+            _owner,
+            _executionDelayBlocks
+        );
+        setUp(initializeParams);
+    }
+
+    /// @dev Initialize function, will be triggered when a new proxy is deployed
+    /// @param initializeParams Parameters of initialization encoded
+    function setUp(bytes memory initializeParams) public override {
+        __Ownable_init();
+        (address _owner, uint256 _executionDelayBlocks) = abi.decode(
+            initializeParams,
+            (address, uint256)
+        );
+
+        transferOwnership(_owner);
+
+        emit VetoGuardSetup(msg.sender, _owner);
+    }
+
+    function queueTransaction(
+        address to,
+        uint256 value,
+        bytes memory data,
+        Enum.Operation operation,
+        uint256 safeTxGas,
+        uint256 baseGas,
+        uint256 gasPrice,
+        address gasToken,
+        address payable refundReceiver,
+        bytes memory signatures,
+        address msgSender
+    ) external {
+        bytes32 transactionHash = getTransactionHash(
+            to,
+            value,
+            data,
+            operation,
+            safeTxGas,
+            baseGas,
+            gasPrice,
+            gasToken,
+            refundReceiver
+        );
+
+        require(
+            transactionData[transactionHash].state == TransactionState.pending,
+            "Transaction must be in the pending state to queue"
+        );
+
+        // todo: Add in checkSignatures to ensure transaction is valid and has been signed
+
+        transactionData[transactionHash].state = TransactionState.queued;
+        transactionData[transactionHash].queuedBlock = block.number;
+    }
+
+    // Only the owner (Fractal veto module) can call this function
+    function vetoTransaction(
+        address to,
+        uint256 value,
+        bytes memory data,
+        Enum.Operation operation,
+        uint256 safeTxGas,
+        uint256 baseGas,
+        uint256 gasPrice,
+        address gasToken,
+        address payable refundReceiver
+    ) external onlyOwner {
+        bytes32 transactionHash = getTransactionHash(
+            to,
+            value,
+            data,
+            operation,
+            safeTxGas,
+            baseGas,
+            gasPrice,
+            gasToken,
+            refundReceiver
+        );
+
+        require(
+            transactionData[transactionHash].state == TransactionState.pending,
+            "Transaction must be in the queued state to veto"
+        );
+        transactionData[transactionHash].state = TransactionState.vetoed;
+    }
+
+    function checkTransaction(
+        address to,
+        uint256 value,
+        bytes memory data,
+        Enum.Operation operation,
+        uint256 safeTxGas,
+        uint256 baseGas,
+        uint256 gasPrice,
+        address gasToken,
+        address payable refundReceiver,
+        bytes memory signatures,
+        address msgSender
+    ) external view override {
+        bytes32 transactionHash = getTransactionHash(
+            to,
+            value,
+            data,
+            operation,
+            safeTxGas,
+            baseGas,
+            gasPrice,
+            gasToken,
+            refundReceiver
+        );
+
+        require(
+            transactionData[transactionHash].state == TransactionState.pending,
+            "Transaction is not in the queued state"
+        );
+        require(
+            block.number >=
+                transactionData[transactionHash].queuedBlock +
+                    executionDelayBlocks,
+            "Transaction delay period has not completed yet"
+        );
+    }
+
+    function checkAfterExecution(bytes32 txHash, bool success)
+        external
+        view
+        override
+    {}
+
+    /// @dev Returns the bytes that are hashed to be signed by owners.
+    /// @param to Destination address.
+    /// @param value Ether value.
+    /// @param data Data payload.
+    /// @param operation Operation type.
+    /// @param safeTxGas Gas that should be used for the safe transaction.
+    /// @param baseGas Gas costs for that are independent of the transaction execution(e.g. base transaction fee, signature check, payment of the refund)
+    /// @param gasPrice Maximum gas price that should be used for this transaction.
+    /// @param gasToken Token address (or 0 if ETH) that is used for the payment.
+    /// @param refundReceiver Address of receiver of gas payment (or 0 if tx.origin).
+    /// @return Transaction hash bytes.
+    function encodeTransactionData(
+        address to,
+        uint256 value,
+        bytes memory data,
+        Enum.Operation operation,
+        uint256 safeTxGas,
+        uint256 baseGas,
+        uint256 gasPrice,
+        address gasToken,
+        address refundReceiver
+    ) public pure returns (bytes memory) {
+        bytes32 safeTxHash = keccak256(
+            abi.encode(
+                SAFE_TX_TYPEHASH,
+                to,
+                value,
+                keccak256(data),
+                operation,
+                safeTxGas,
+                baseGas,
+                gasPrice,
+                gasToken,
+                refundReceiver
+            )
+        );
+        return
+            abi.encodePacked(
+                bytes1(0x19),
+                bytes1(0x01),
+                DOMAIN_SEPARATOR_TYPEHASH,
+                safeTxHash
+            );
+    }
+
+    /// @dev Returns hash to be signed by owners.
+    /// @param to Destination address.
+    /// @param value Ether value.
+    /// @param data Data payload.
+    /// @param operation Operation type.
+    /// @param safeTxGas Fas that should be used for the safe transaction.
+    /// @param baseGas Gas costs for data used to trigger the safe transaction.
+    /// @param gasPrice Maximum gas price that should be used for this transaction.
+    /// @param gasToken Token address (or 0 if ETH) that is used for the payment.
+    /// @param refundReceiver Address of receiver of gas payment (or 0 if tx.origin).
+    /// @return Transaction hash.
+    function getTransactionHash(
+        address to,
+        uint256 value,
+        bytes memory data,
+        Enum.Operation operation,
+        uint256 safeTxGas,
+        uint256 baseGas,
+        uint256 gasPrice,
+        address gasToken,
+        address refundReceiver
+    ) public view returns (bytes32) {
+        return
+            keccak256(
+                encodeTransactionData(
+                    to,
+                    value,
+                    data,
+                    operation,
+                    safeTxGas,
+                    baseGas,
+                    gasPrice,
+                    gasToken,
+                    refundReceiver
+                )
+            );
+    }
+
+    function getTransactionState(
+        address to,
+        uint256 value,
+        bytes memory data,
+        Enum.Operation operation,
+        uint256 safeTxGas,
+        uint256 baseGas,
+        uint256 gasPrice,
+        address gasToken,
+        address payable refundReceiver
+    ) public view returns (TransactionState) {
+        return
+            transactionData[
+                getTransactionHash(
+                    to,
+                    value,
+                    data,
+                    operation,
+                    safeTxGas,
+                    baseGas,
+                    gasPrice,
+                    gasToken,
+                    refundReceiver
+                )
+            ].state;
+    }
+
+    function getTransactionQueuedBlock(
+        address to,
+        uint256 value,
+        bytes memory data,
+        Enum.Operation operation,
+        uint256 safeTxGas,
+        uint256 baseGas,
+        uint256 gasPrice,
+        address gasToken,
+        address payable refundReceiver
+    ) public view returns (uint256) {
+        return
+            transactionData[
+                getTransactionHash(
+                    to,
+                    value,
+                    data,
+                    operation,
+                    safeTxGas,
+                    baseGas,
+                    gasPrice,
+                    gasToken,
+                    refundReceiver
+                )
+            ].queuedBlock;
+    }
+}

e7cfc0ad2552e1ac8c020448eabe4567531b147b
tbwebb22 <tbwebb22@gmail.com>
2022-09-14 15:58:51 -0400

    Create VetoERC20Voting.sol

---
+++ contracts/VetoERC20Voting.sol
@@ -0,0 +1,242 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "./interfaces/IERC20Votes.sol";
+import "./interfaces/IVetoGuard.sol";
+import "@gnosis.pm/safe-contracts/contracts/common/Enum.sol";
+
+contract VetoVoting {
+    uint256 public vetoVotesThreshold;
+    address public gnosisSafe;
+    IERC20Votes public votesToken;
+    IVetoGuard public vetoGuard;
+    mapping(bytes32 => uint256) public transactionVetoVotes;
+    mapping(address => mapping(bytes32 => bool)) public userHasVoted;
+
+    // todo: need to look into these two values and if we need it in this contract
+    bytes32 private constant SAFE_TX_TYPEHASH =
+        0xbb8310d486368db6bd6f849402fdd73ad53d316b5a4b2644ad6efe0f941286d8;
+    bytes32 private constant DOMAIN_SEPARATOR_TYPEHASH =
+        0x47e79534a245952e8b16893a336b85a3d9ea9fa8c573f3d803afb92a79469218;
+
+    constructor(
+        uint256 _vetoVotesThreshold,
+        address _gnosisSafe,
+        address _votesToken,
+        address _vetoGuard
+    ) {
+        vetoVotesThreshold = _vetoVotesThreshold;
+        gnosisSafe = _gnosisSafe;
+        votesToken = IERC20Votes(_votesToken);
+        vetoGuard = IVetoGuard(_vetoGuard);
+    }
+
+    function castVetoVote(
+        address to,
+        uint256 value,
+        bytes memory data,
+        Enum.Operation operation,
+        uint256 safeTxGas,
+        uint256 baseGas,
+        uint256 gasPrice,
+        address gasToken,
+        address payable refundReceiver
+    ) external {
+        // Get the transaction hash
+        bytes32 transactionHash = getTransactionHash(
+            to,
+            value,
+            data,
+            operation,
+            safeTxGas,
+            baseGas,
+            gasPrice,
+            gasToken,
+            refundReceiver
+        );
+
+        // Check that user has not yet voted
+        require(
+            !userHasVoted[msg.sender][transactionHash],
+            "User has already voted"
+        );
+
+        // Check that the transaction has been queued
+        require(
+            vetoGuard.getTransactionState(
+                to,
+                value,
+                data,
+                operation,
+                safeTxGas,
+                baseGas,
+                gasPrice,
+                gasToken,
+                refundReceiver
+            ) == IVetoGuard.TransactionState.queued,
+            "Transaction has not yet been queued"
+        );
+
+        // check the block number the transaction was queued on the VetoGuard
+        uint256 queuedBlockNumber = vetoGuard.getTransactionQueuedBlock(
+            to,
+            value,
+            data,
+            operation,
+            safeTxGas,
+            baseGas,
+            gasPrice,
+            gasToken,
+            refundReceiver
+        );
+
+        // get the number of the user's votes at that block
+        uint256 userVotes = votesToken.getPastVotes(
+            msg.sender,
+            queuedBlockNumber
+        );
+
+        // Add votes to the data structure
+        transactionVetoVotes[transactionHash] += userVotes;
+
+        // User has voted
+        userHasVoted[msg.sender][transactionHash] = true;
+    }
+
+    /// @dev Returns the bytes that are hashed to be signed by owners.
+    /// @param to Destination address.
+    /// @param value Ether value.
+    /// @param data Data payload.
+    /// @param operation Operation type.
+    /// @param safeTxGas Gas that should be used for the safe transaction.
+    /// @param baseGas Gas costs for that are independent of the transaction execution(e.g. base transaction fee, signature check, payment of the refund)
+    /// @param gasPrice Maximum gas price that should be used for this transaction.
+    /// @param gasToken Token address (or 0 if ETH) that is used for the payment.
+    /// @param refundReceiver Address of receiver of gas payment (or 0 if tx.origin).
+    /// @return Transaction hash bytes.
+    function encodeTransactionData(
+        address to,
+        uint256 value,
+        bytes memory data,
+        Enum.Operation operation,
+        uint256 safeTxGas,
+        uint256 baseGas,
+        uint256 gasPrice,
+        address gasToken,
+        address refundReceiver
+    ) public pure returns (bytes memory) {
+        bytes32 safeTxHash = keccak256(
+            abi.encode(
+                SAFE_TX_TYPEHASH,
+                to,
+                value,
+                keccak256(data),
+                operation,
+                safeTxGas,
+                baseGas,
+                gasPrice,
+                gasToken,
+                refundReceiver
+            )
+        );
+        return
+            abi.encodePacked(
+                bytes1(0x19),
+                bytes1(0x01),
+                DOMAIN_SEPARATOR_TYPEHASH,
+                safeTxHash
+            );
+    }
+
+    /// @dev Returns hash to be signed by owners.
+    /// @param to Destination address.
+    /// @param value Ether value.
+    /// @param data Data payload.
+    /// @param operation Operation type.
+    /// @param safeTxGas Fas that should be used for the safe transaction.
+    /// @param baseGas Gas costs for data used to trigger the safe transaction.
+    /// @param gasPrice Maximum gas price that should be used for this transaction.
+    /// @param gasToken Token address (or 0 if ETH) that is used for the payment.
+    /// @param refundReceiver Address of receiver of gas payment (or 0 if tx.origin).
+    /// @return Transaction hash.
+    function getTransactionHash(
+        address to,
+        uint256 value,
+        bytes memory data,
+        Enum.Operation operation,
+        uint256 safeTxGas,
+        uint256 baseGas,
+        uint256 gasPrice,
+        address gasToken,
+        address refundReceiver
+    ) public pure returns (bytes32) {
+        return
+            keccak256(
+                encodeTransactionData(
+                    to,
+                    value,
+                    data,
+                    operation,
+                    safeTxGas,
+                    baseGas,
+                    gasPrice,
+                    gasToken,
+                    refundReceiver
+                )
+            );
+    }
+
+    function getIsVetoed(
+        address to,
+        uint256 value,
+        bytes memory data,
+        Enum.Operation operation,
+        uint256 safeTxGas,
+        uint256 baseGas,
+        uint256 gasPrice,
+        address gasToken,
+        address payable refundReceiver
+    ) external view returns (bool) {
+        // Get the transaction hash
+        bytes32 transactionHash = getTransactionHash(
+            to,
+            value,
+            data,
+            operation,
+            safeTxGas,
+            baseGas,
+            gasPrice,
+            gasToken,
+            refundReceiver
+        );
+
+        return transactionVetoVotes[transactionHash] > vetoVotesThreshold;
+    }
+
+    function getVetoVotes(
+        address to,
+        uint256 value,
+        bytes memory data,
+        Enum.Operation operation,
+        uint256 safeTxGas,
+        uint256 baseGas,
+        uint256 gasPrice,
+        address gasToken,
+        address payable refundReceiver
+    ) external view returns (uint256) {
+        // Get the transaction hash
+        bytes32 transactionHash = getTransactionHash(
+            to,
+            value,
+            data,
+            operation,
+            safeTxGas,
+            baseGas,
+            gasPrice,
+            gasToken,
+            refundReceiver
+        );
+
+        return transactionVetoVotes[transactionHash];
+    }
+}

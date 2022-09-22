2210e45e6feaf236381ac75f2df46ec09b59f2b4
Christopher Dancy <chris.dancy@decentlabs.io>
2022-09-22 09:38:33 -0400

    import gnosis interface for easy contract interation

--- contracts/interfaces/IGnosisSafe.sol
+++ contracts/interfaces/IGnosisSafe.sol
@@ -4,7 +4,27 @@ pragma solidity ^0.8.0;
 import "@gnosis.pm/safe-contracts/contracts/common/Enum.sol";
 
 interface IGnosisSafe {
-    function nonce() external view returns(uint256);
+    function nonce() external view returns (uint256);
+
+    /// @dev Setup function sets initial storage of contract.
+    /// @param _owners List of Safe owners.
+    /// @param _threshold Number of required confirmations for a Safe transaction.
+    /// @param to Contract address for optional delegate call.
+    /// @param data Data payload for optional delegate call.
+    /// @param fallbackHandler Handler for fallback calls to this contract
+    /// @param paymentToken Token that should be used for the payment (0 is ETH)
+    /// @param payment Value that should be paid
+    /// @param paymentReceiver Address that should receive the payment (or 0 if tx.origin)
+    function setup(
+        address[] calldata _owners,
+        uint256 _threshold,
+        address to,
+        bytes calldata data,
+        address fallbackHandler,
+        address paymentToken,
+        uint256 payment,
+        address payable paymentReceiver
+    ) external;
 
     /**
      * @dev Checks whether the signature provided is valid for the provided data, hash. Will revert otherwise.

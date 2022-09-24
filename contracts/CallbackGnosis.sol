d20d7dbf3a90746e546046662033153140d7f659
Christopher Dancy <chris.dancy@decentlabs.io>
2022-09-24 09:59:08 -0400

    created seperate functions - created multisend which bypasses the gnosis safe guard

--- contracts/CallbackGnosis.sol
+++ contracts/CallbackGnosis.sol
@@ -15,20 +15,48 @@ contract CallbackGnosis {
         );
 
         (
-            address[] memory targets,
-            bytes[] memory datas,
-            bool[] memory gnosisExecTx
-        ) = abi.decode(txData, (address[], bytes[], bool[]));
-        // I should send multiple tx - same time
+            address[][] memory targets,
+            bytes[][] memory txs,
+            bool[] memory gnosisExecTxs
+        ) = abi.decode(txData, (address[][], bytes[][], bool[]));
+
         for (uint256 i; i < targets.length; i++) {
-            if (gnosisExecTx[i]) {
-                (bool success, ) = address(proxy).call(
+            if (gnosisExecTxs[i]) {
+                gnosisExecTx(targets[i], txs[i], proxy, signature);
+            } else {
+                for(uint j; j < targets[i].length; j++) {
+                    // could this be a multisend as well?
+                    txCall(targets[i][j], txs[i][j], proxy);
+                }
+            }
+        }
+    }
+
+    function multiSend(
+        address[] memory _targets,
+        bytes[] memory _datas,
+        address _proxy
+    ) public {
+        for (uint256 i; i < _targets.length; i++) {
+            (bool success, ) = address(_targets[i] == address(0) ? _proxy : _targets[i])
+                .call(_datas[i]);
+                require(success, "CB001");
+        }
+    }
+
+    function gnosisExecTx(address[] memory targets, bytes[] memory txs, address proxy, bytes memory signature) internal {
+        (bool success, ) = address(proxy).call(
                     abi.encodeWithSignature(
                         "execTransaction(address,uint256,bytes,uint8,uint256,uint256,uint256,address,address,bytes)",
-                        targets[i] == address(0) ? proxy : targets[i],
+                        address(this), // multisend address
                         0,
-                        datas[i],
-                        0,
+                        abi.encodeWithSignature(
+                            "multiSend(address[],bytes[],address)",
+                            targets,
+                            txs,
+                            proxy
+                        ), // data
+                        1,
                         0,
                         0,
                         0,
@@ -38,12 +66,13 @@ contract CallbackGnosis {
                     )
                 );
                 require(success, "CB000");
-            } else {
+    }
+
+    function txCall(address _target, bytes memory _tx, address proxy) internal {
                 (bool success, ) = address(
-                    targets[i] == address(0) ? proxy : targets[i]
-                ).call(datas[i]);
-                require(success, "CB000");
-            }
-        }
+                    _target == address(0) ? proxy : _target
+                ).call(_tx);
+                require(success, "CB002");
+               
     }
 }

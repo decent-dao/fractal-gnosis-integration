c4fab3de33abe9dae50887d701b7abc7a241a856
Christopher Dancy <chris.dancy@decentlabs.io>
2022-09-27 16:41:51 -0400

    utilize gnosis pm interface

--- contracts/interfaces/ICallbackGnosis.sol
+++ contracts/interfaces/ICallbackGnosis.sol
@@ -1,21 +1,8 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
-import "./IProxyCreationCallback.sol";
-
+import "@gnosis.pm/safe-contracts/contracts/proxies/IProxyCreationCallback.sol";
 
 interface ICallbackGnosis is IProxyCreationCallback {
-    /// @dev Method called once a proxy contract is created
-    /// @param proxy GnosisSafe address
-    /// @param _singleton GnosisSafe impl address
-    /// @param initializer Payload used to setup GnosisSafe Configuration
-    /// @param saltNonce Salt utilized for GnosisSafe Create2 opcode
-    function proxyCreated(
-        address proxy,
-        address _singleton,
-        bytes calldata initializer,
-        uint256 saltNonce
-    ) external;
-
     /// @notice Allows Gnosis Safe txs without knowledge of the Gnosis address
     /// @dev Utilized to bypass the txGuard / Sig Requirement
     /// @param _targets Contract Address / Address(0) == proxy

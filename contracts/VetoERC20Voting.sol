b10318f561bd53a44c16fdaf644209ccd4370c58
tbwebb22 <tbwebb22@gmail.com>
2022-09-27 13:45:55 -0400

    Add getFreezeVotes function

--- contracts/VetoERC20Voting.sol
+++ contracts/VetoERC20Voting.sol
@@ -153,4 +153,42 @@ contract VetoERC20Voting is IVetoERC20Voting, TransactionHasher, Initializable {
                 )
             ];
     }
+
+    /// @notice Returns the number of votes that have been cast to freeze the DAO
+    /// @param to Destination address.
+    /// @param value Ether value.
+    /// @param data Data payload.
+    /// @param operation Operation type.
+    /// @param safeTxGas Gas that should be used for the safe transaction.
+    /// @param baseGas Gas costs for that are independent of the transaction execution(e.g. base transaction fee, signature check, payment of the refund)
+    /// @param gasPrice Maximum gas price that should be used for this transaction.
+    /// @param gasToken Token address (or 0 if ETH) that is used for the payment.
+    /// @param refundReceiver Address of receiver of gas payment (or 0 if tx.origin).
+    /// @return uint256 The number of freeze votes that have been cast
+    function getFreezeVotes(
+        address to,
+        uint256 value,
+        bytes memory data,
+        Enum.Operation operation,
+        uint256 safeTxGas,
+        uint256 baseGas,
+        uint256 gasPrice,
+        address gasToken,
+        address payable refundReceiver
+    ) external view returns (uint256) {
+        return
+            transactionFreezeVotes[
+                getTransactionHash(
+                    to,
+                    value,
+                    data,
+                    operation,
+                    safeTxGas,
+                    baseGas,
+                    gasPrice,
+                    gasToken,
+                    refundReceiver
+                )
+            ];
+    }
 }
